579c1014f80b01b31a234973b6e01618b9822051
Vadim <vadim.shikulo@yellow.id>
2017-12-27 20:11:52 +0300

    Fix crash https://github.com/jessesquires/JSQMessagesViewController/issues/2128

--- JSQMessagesViewController/Views/JSQMessagesComposerTextView.m
+++ JSQMessagesViewController/Views/JSQMessagesComposerTextView.m
@@ -25,8 +25,6 @@
 
 @implementation JSQMessagesComposerTextView
 
-@synthesize pasteDelegate;
-
 #pragma mark - Initialization
 
 - (void)jsq_configureTextView
@@ -144,7 +142,7 @@
 
 - (void)paste:(id)sender
 {
-    if (!self.pasteDelegate || [self.pasteDelegate composerTextView:self shouldPasteWithSender:sender]) {
+    if (!self.jsqPasteDelegate || [self.jsqPasteDelegate composerTextView:self shouldPasteWithSender:sender]) {
         [super paste:sender];
     }
 }
